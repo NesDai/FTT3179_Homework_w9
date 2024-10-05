aa2716a34a1a0b0059a3c80db14f4d1cbfd65ee0
NesDai <tche0082@student.monash.edu>
2024-10-05 19:51:18 +0800

    ewewf

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,5 +1,26 @@
 var spec2 = "js/map.json";
-vegaEmbed("#bar_chart", spec2)
+vegaEmbed("#chart_1", spec2)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+// var spec3 = "js/bar_chart.json";
+// vegaEmbed("#chart_2", spec3)
+//   .then(function (result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+//   })
+//   .catch(console.error);
+
+// var spec4 = "js/bar_chart_2.json";
+// vegaEmbed("#chart_3", spec4)
+//   .then(function (result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+//   })
+//   .catch(console.error);
+
+var spec5 = "js/combined_chart.json";
+vegaEmbed("#chart_4", spec5)
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
